b44f7887fe5d4275a1a78ed3d7924d88933c2b28
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-16 04:09:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js"; 
+const app = express();
+//dotenv.config();
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user",userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello to my memories API");
+});
+//const CONNECTION_URL =  "mongodb+srv://pro-memory:pro-memory123@cluster0.kl6wa.mongodb.net/postmessages";
+const PORT = process.env.PORT || 7000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
