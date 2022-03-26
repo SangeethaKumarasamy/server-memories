ce14fb8d18153cc2386975ea446bb037ee409853
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-26 11:44:31 +0530

    commit

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js"; 
 const app = express();
-//dotenv.config();
 dotenv.config();
 
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
